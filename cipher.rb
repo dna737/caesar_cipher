ba4454f3cb10b19aa361b2d74450940a80696a83
dna737 <adnanhere27@gmail.com>
2023-02-09 10:44:20 +0530

    clean redundant changes

--- cipher.rb
+++ cipher.rb
@@ -1,36 +1,36 @@
 def caesar_cipher(word = "Hello world!", shift = 0)
-    new_word = ""
-    #array of characters:
-    word.split('').each do |c|
-    if (c.ord >= 97 && c.ord <= 122) || (c.ord >= 65 && c.ord <= 90) then 
-        if is_upper?(c) then 
-            new_word += regulate_ord((c.downcase.ord+shift)).upcase
-        else
+  new_word = ""
+  # array of characters:
+  word.split('').each do |c|
+    if (c.ord >= 97 && c.ord <= 122) || (c.ord >= 65 && c.ord <= 90) then
+      if is_upper?(c) then
+        new_word += regulate_ord((c.downcase.ord + shift)).upcase
+      else
         new_word += regulate_ord((c.ord + shift))
-        end
+      end
     else
-    new_word += c
-    next
+      new_word += c
+      next
     end
-end
-new_word
+  end
+  new_word
 end
 
-def is_upper?(char) 
-    char == char.upcase
+def is_upper?(char)
+  char == char.upcase
 end
 
 def regulate_ord(char_num)
-    number = nil
-    if char_num < 97 then 
-        number = 122-char_num+96
-        number.chr
-    elsif char_num > 122 then
-        number = char_num-122+96
-        number.chr
-    else
-        char_num.chr
-    end
+  number = nil
+  if char_num < 97 then
+    number = 122 - char_num + 96
+    number.chr
+  elsif char_num > 122 then
+    number = char_num - 122 + 96
+    number.chr
+  else
+    char_num.chr
+  end
 end
 
 puts "Hi! please enter the word and the offset:"
